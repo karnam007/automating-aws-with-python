230830641d397de82b240a8a8482863befad6e72
karnam007 <karnam007@gmail.com>
2018-11-09 15:24:18 +0530

    refactored code

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -1,29 +1,45 @@
-import boto3
-import sys
-import click
-from botocore.exceptions import ClientError
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+Webotron script automates the process of deploying webiste to S3. It automates the whole process
+1. Configure AWS S3 buckets
+2. Creating a new S3 buckets
+3. Set them up for static website hosting
+4. deploying static website assests
+5. Configure DNS Route 53
+6. Configure CDN and SSL
+'''
+
+
 from pathlib import Path
 import mimetypes
+import boto3
+from botocore.exceptions import ClientError
+import sys
+import click
 
 
 session = boto3.Session(profile_name='pythonAutomation')
-s3=session.resource('s3')
+s3 = session.resource('s3')
+
 
 @click.group()
 def cli():
-    "Webotron deploys websites to AWS"
+    '''Webotron deploys websites to AWS'''
     pass
+
 
 @cli.command('list-buckets')
 def list_buckets():
-    "List all s3 buckets"
+    '''List all s3 buckets'''
     for b1 in s3.buckets.all():
         print(b1)
+
 
 @cli.command('list-buckets-objects')
 @click.argument('bucket')
 def list_buckets_objects(bucket):
-    "List all of S3 objects"
+    '''List all of S3 objects'''
     for obj in s3.Bucket(bucket).objects.all():
         print("obj")
 
@@ -31,7 +47,7 @@ def list_buckets_objects(bucket):
 @cli.command('setup-bucket')
 @click.argument('bucket')
 def setup_bucket(bucket):
-    "Create and configure S3 buckets"
+    '''Create and configure S3 buckets'''
     s3_bucket = None
     try:
         s3_bucket = s3.create_bucket(Bucket=bucket, CreateBucketConfiguration={'LocationConstraint': session.region_name})
@@ -81,6 +97,7 @@ def upload_file(s3_bucket,path, key):
 @click.argument('bucket')
 
 def sync(pathname,bucket):
+    '''This is to upload the contents to AWS S3'''
     pass
     s3_bucket= s3.Bucket(bucket)
     root = Path(pathname).expanduser().resolve()
